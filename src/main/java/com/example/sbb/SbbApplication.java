ff1b412572f4bc38d7609f7a353bf4cb2d2ec997
Rayched <ny2401@naver.com>
2023-07-23 12:36:23 +0900

    git repository Connect Success

--- src/main/java/com/example/sbb/SbbApplication.java
+++ src/main/java/com/example/sbb/SbbApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbbApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(SbbApplication.class, args);
 	}
-
+	//기본 port는 8080이다.
+	//해당 port로 접속이 되지 않을 경우에 application.properties에서
+	//port 번호를 변경할 것
+	//"localhost:8080/"
 }
